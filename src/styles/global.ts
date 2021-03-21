0f65f0480401862bd726b79b974c959924eec753
Matheus Luiz da Silva <lsmatheus94@gmail.com>
2021-03-21 18:16:28 -0300

    feat: add some global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eee;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     box-sizing: border-box;
     margin: 0;
@@ -14,11 +26,22 @@ const GlobalStyles = createGlobalStyle`
   html,
   body,
   #__next {
+    background: var(--background);
+    color: var(--white);
     height: 100%;
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  p {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `;
 
